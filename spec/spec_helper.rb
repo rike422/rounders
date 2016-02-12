5428d479f4b20b52ae09e1beb7813a81b978c9a6
akira.takahashi <rike422@gmail.com>
2016-02-13 02:04:05 +0900

    Inspection code coverage on CI by code-climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'kiki'
+require 'simplecov'
+SimpleCov.start
+
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  SimpleCov.coverage_dir(dir)
+
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
