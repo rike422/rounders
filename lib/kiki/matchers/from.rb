eae561f7e1badc2f6b85a7c2832e18d0de2cd177
akira.takahashi <rike422@gmail.com>
2016-02-17 19:55:19 +0900

    Add skelton Mail#from matcher

---
+++ lib/kiki/matchers/from.rb
@@ -0,0 +1,5 @@
+module Kiki
+  module Matchers
+    class From < Kiki::Matchers::Matcher; end
+  end
+end
---
+++ spec/kiki/matchers/from_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe Kiki::Matchers::From do
+  let(:described_class) { Kiki::Matchers::From }
+  let(:described_instance) { described_class.new(*arguments) }
+  let(:arguments) { [] }
+
+  describe '#inherited' do
+    subject { described_class }
+    it { is_expected.to be_a Kiki::Matchers::Matcher }
+  end
+
+  describe '.new' do
+    subject { described_instance }
+    it { is_expected.to be_a described_class }
+
+    context 'missing pattern' do
+      let(:arguments) do
+        {}
+      end
+      it 'should raise ArgumentError' do
+        expect { described_instance }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '#match' do
+    context 'when Message.from is a Array' do
+      let(:arguments) do
+        {
+          pattern: /github.com/
+        }
+      end
+      let(:message) do
+        instance_double('Message', from: ['rike422@github.com', 'rike422+2@github.com'])
+      end
+      it 'should return Array of MatchData' do
+        expect(described_incetane.match(message)).to_not be_a(Array)
+        expect(described_incetane.match(message)).to all be_a(MatchData)
+      end
+    end
+    context 'when Message.from be a string' do
+      let(:arguments) do
+        {
+          pattern: /github.com/
+        }
+      end
+      let(:message) do
+        instance_double('Message', from: 'rike422@github.com')
+      end
+      it 'should return Array of MatchData' do
+        expect(described_incetane.match(message)).to_not be_a(Array)
+      end
+    end
+  end
+end
