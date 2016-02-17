659f70402a0cd7bdf73b036ce86146c734c03c04
akira.takahashi <rike422@gmail.com>
2016-02-17 20:16:16 +0900

    Fix require files

--- lib/kiki.rb
+++ lib/kiki.rb
@@ -1,12 +1,19 @@
-require "kiki/version"
 require 'kiki/version'
 
 module Kiki
   # Your code goes here...
 end
+
+require 'kiki/configuration'
+require 'kiki/mail'
+require 'kiki/rounder'
 require 'kiki/actions/base'
 require 'kiki/brains/base'
 require 'kiki/handlers/base'
+
 require 'kiki/matchers/matcher'
+require 'kiki/matchers/subject'
 require 'kiki/matchers/body'
 require 'kiki/matchers/cc'
+require 'kiki/matchers/from'
+require 'kiki/matchers/to'
