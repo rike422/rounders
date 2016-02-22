8dcc41dd6fc70a743bb13a5cab9ce085b3d03898
akira.takahashi <rike422@gmail.com>
2016-02-22 12:56:31 +0900

    Implement Kiki::Matchers:From

--- lib/kiki/matchers/from.rb
+++ lib/kiki/matchers/from.rb
@@ -1,5 +1,18 @@
 module Kiki
   module Matchers
-    class From < Kiki::Matchers::Matcher; end
+    class From < Kiki::Matchers::Matcher
+      attr_reader :pattern
+
+      def initialize(pattern)
+        @pattern = pattern
+      end
+
+      def match(message)
+        matches = message.from.map do |address|
+          address.match(pattern)
+        end
+        matches.compact
+      end
+    end
   end
 end
