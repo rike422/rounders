386ad5cf7da4afbd13ed11f89d55b450e6ddf5a5
akira.takahashi <rike422@gmail.com>
2016-03-02 17:26:16 +0900

    Implement Kiki::Receiver

--- lib/kiki/receiver.rb
+++ lib/kiki/receiver.rb
@@ -1,3 +1,72 @@
 module Kiki
-  class Receiver; end
+  class Receiver
+    class Config
+      attr_accessor :protocol
+      attr_accessor :option
+    end
+
+    attr_reader :client
+
+    def initialize(client: nil)
+      @client = client
+    end
+
+    def receive
+      @client.
+        find(keys: %w(NOT SEEN)).
+        map { |message| Kiki::Mail.new(message) }
+    end
+
+    def configure
+      config = Config.new
+      yield config
+      @client = Receiver.create_client(config)
+    end
+
+    class << self
+      def add
+        receiver = new
+        sub_receivers.push(receiver)
+        receiver
+      end
+
+      def configure
+        config = Config.new
+        yield config
+        @receiver = create_client(config)
+      end
+
+      def create_client(config)
+        retriever = parser.lookup_retriever_method(config.protocol)
+        retriever.new(config.option)
+      end
+
+      def receive
+        receivers.
+          map(&:receive).
+          flatten.
+          sort { |a, b| a.date <=> b.date }
+      end
+
+      def reset
+        @receiver      = nil
+        @sub_receivers = nil
+        @receivers     = nil
+      end
+
+      private
+
+      def receivers
+        @receivers ||= [@receiver].concat(sub_receivers)
+      end
+
+      def sub_receivers
+        @sub_receivers ||= []
+      end
+
+      def parser
+        ::Mail::Configuration.instance
+      end
+    end
+  end
 end
--- spec/kiki/receiver_spec.rb
+++ spec/kiki/receiver_spec.rb
@@ -4,12 +4,117 @@ describe Kiki::Receiver do
   let(:described_class) { Kiki::Receiver }
   let(:described_instance) { described_class.new(*arguments) }
   let(:arguments) { [] }
+  let(:config) { Kiki::Receiver::Config.new }
+  let(:messages) do
+    (1..10).map do |i|
+      ::Mail.new(
+        to:      "someone#{i}@somewhere.com",
+        body:    "body_#{i}",
+        subject: "subject#{i}",
+        date:    i.days.ago
+      )
+    end
+  end
+  before(:each) do
+    described_class.reset
+  end
+  describe '.add' do
+    it 'should be a Kiki::Receiver' do
+      expect(described_class.add).to be_a described_class
+    end
+    it 'should push Kiki::Receiver to @receiver' do
+      expect { described_class.add }.to change { described_class.send(:sub_receivers).length }.by(+1)
+    end
+  end
+
+  describe '.configure' do
+    it 'should yield with Kiki::Receiver::Config' do
+      expect(described_class).to receive(:create_client).with(be_a(Kiki::Receiver::Config))
+      expect { |b| described_class.configure(&b) }.to yield_with_args(Kiki::Receiver::Config)
+    end
+  end
+
+  describe '.create_client' do
+    before(:each) do
+      config.protocol = :pop3
+      config.option   = {
+        address:        'localhost',
+        port:           110,
+        user_name:      'rike422',
+        password:       nil,
+        authentication: nil,
+        enable_ssl:     true
+      }
+    end
+    let(:retriever) { Mail::POP3 }
+    it 'should call Mail::Configuration::lookup_retriever_method' do
+      expect_any_instance_of(::Mail::Configuration).to receive(:lookup_retriever_method).with(config.protocol).and_call_original
+      described_class.create_client(config)
+    end
+    it 'should create Mail::Retriever with' do
+      expect(retriever).to receive(:new).with(config.option)
+      allow_any_instance_of(Mail::Configuration).to receive(:lookup_retriever_method).and_return(retriever)
+      described_class.create_client(config)
+    end
+  end
 
   describe '.new' do
     subject { described_instance }
     it { is_expected.to be_a described_class }
   end
 
+  describe '.receive' do
+    let(:main_receiver) do
+      retriever = ::Mail::POP3.new({})
+      allow(retriever).to receive(:find).and_return messages[0..5]
+      Kiki::Receiver.new(client: retriever)
+    end
+    let(:sub_receivers) do
+      (messages - messages[0..5]).reverse.map do |message|
+        retriever = ::Mail::POP3.new({})
+        allow(retriever).to receive(:find).and_return([message])
+        Kiki::Receiver.new(client: retriever)
+      end
+    end
+    before(:each) do
+      described_class.instance_variable_set(:@receiver, main_receiver)
+      described_class.instance_variable_set(:@sub_receivers, sub_receivers)
+    end
+
+    subject { described_class.receive }
+    it { is_expected.to be_a Array }
+    it { is_expected.to all be_a(Kiki::Mail) }
+    it 'should return Kiki::Mail, that sort_by date asc' do
+      expect(described_class.receive.map(&:date)).to eq messages.reverse.map(&:date)
+      described_class.receive.map(&:date)
+    end
+  end
+
+  describe '#configure' do
+    it 'should yield with Kiki::Receiver::Config' do
+      spy = instance_spy('client')
+      expect(described_class).to receive(:create_client).with(be_a(Kiki::Receiver::Config)).and_return spy
+      expect { |b| described_instance.configure(&b) }.to yield_with_args(Kiki::Receiver::Config)
+      expect(described_instance.client).to eq spy
+    end
+  end
+
   describe '#receive' do
+    let(:client) { ::Mail::POP3.new({}) }
+    let(:arguments) do
+      [
+        { client: client }
+      ]
+    end
+    before(:each) do
+      allow(client).to receive(:find).and_return(messages)
+    end
+    subject { described_instance.receive }
+    it { is_expected.to be_a Array }
+    it { is_expected.to all be_a(Kiki::Mail) }
+    it 'should find unread email' do
+      expect(client).to receive(:find).with(keys: %w(NOT SEEN))
+      described_instance.receive
+    end
   end
 end
