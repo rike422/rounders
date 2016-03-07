8dccf1b9f8b5bf9ccc6c7dbadb352a08245cb421
akira.takahashi <rike422@gmail.com>
2016-03-08 00:17:59 +0900

    Change Kiki::Pluggable now extend util methods

--- lib/kiki/pluggable.rb
+++ lib/kiki/pluggable.rb
@@ -3,24 +3,25 @@ module Kiki
   module Pluggable
     PLUGIN_BASE_DIR_PATH = 'plugins'.freeze
     class << self
-      def included(child)
-        load_plugins(child)
+      def included(klass)
+        klass.extend Kiki::Pluggable::ClassMethods
+        klass.load_plugins
+      end
+    end
+
+    module ClassMethods
+      def directory_name
+        @directory_name ||= name.downcase.pluralize
       end
 
-      private
+      def load_path
+        @load_path ||= "#{PLUGIN_BASE_DIR_PATH}/#{directory_name}"
+      end
 
-      def load_plugins(child)
-        Pathname.glob(load_path(child)).each do |plugin|
+      def load_plugins
+        Pathname.glob(load_path).each do |plugin|
           require_relative plugin.expand_path
         end
-      end
-
-      def load_path(child)
-        "#{PLUGIN_BASE_DIR_PATH}/#{directory_name(child)}"
-      end
-
-      def directory_name(child)
-        child.name.downcase.pluralize
       end
     end
   end
