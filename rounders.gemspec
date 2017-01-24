47992b9f7577ecf534593d2304441ed702e67516
akira.takahashi <rike422@gmail.com>
2017-01-24 13:12:28 +0900

    Update CI

--- rounders.gemspec
+++ rounders.gemspec
@@ -23,7 +23,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rspec', '~> 3.0'
   spec.add_development_dependency 'simplecov'
   spec.add_development_dependency 'coveralls'
-  spec.add_development_dependency 'codeclimate-test-reporter'
   spec.add_development_dependency 'guard'
   spec.add_development_dependency 'guard-rspec'
   spec.add_development_dependency 'guard-rubocop', '~> 1.2.0'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,18 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'support/custom_matcher/delegate_matcher'
-if ENV['CIRCLE_ARTIFACTS']
+if ENV['CI']
   require 'simplecov'
   require 'coveralls'
-  require 'codeclimate-test-reporter'
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-    SimpleCov::Formatter::HTMLFormatter,
-    Coveralls::SimpleCov::Formatter
-  ]
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-  SimpleCov.coverage_dir(dir)
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter
+    ]
+  )
   SimpleCov.start do
     add_group 'lib', 'lib'
     add_filter '/vendor/'
   end
-  CodeClimate::TestReporter.start
 end
 
 require 'rounders'
