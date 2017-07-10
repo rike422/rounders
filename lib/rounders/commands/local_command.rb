996c89bbf28bf0250b3c48c3c3d620c5f7215455
akira.takahashi <rike422@gmail.com>
2017-07-10 14:07:55 +0900

    Add version command

--- lib/rounders/commands/local_command.rb
+++ lib/rounders/commands/local_command.rb
@@ -2,6 +2,7 @@ module Rounders
   module Commands
     class LocalCommand < Thor
       class_option :help, type: :boolean, aliases: '-h', desc: 'Help message.'
+      class_option :version, type: :boolean, desc: 'version'
       package_name 'rounders'
 
       desc 'start', 'Start the Rounders'
@@ -9,6 +10,7 @@ module Rounders
       method_option :dotenv, type: :boolean, default: false
       method_option :daemon, type: :boolean, default: false
       method_option :pid, type: :string
+
       def start
         rounder = Rounders::Rounder.new(options)
         rounder.start
@@ -17,6 +19,11 @@ module Rounders
       desc 'generate [Type]', 'Generate new code'
       method_option aliases: '-g'
       subcommand 'generate', SubCommands::Generate
+
+      desc 'version', 'Show rounders versoin'
+      def version
+        puts Rounders::VERSION
+      end
     end
   end
 end
