9924e997af430ae986ee0b1a2d2f0f84f03a1503
akira.takahashi <rike422@gmail.com>
2017-07-10 13:14:36 +0900

    Add aruba gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,3 +34,12 @@ require 'rounders'
 require 'rounders/commands/sub_commands/generate'
 require 'rounders/commands/local_command'
 require 'rounders/commands/global_command'
+
+
+if RUBY_VERSION < '1.9.3'
+  ::Dir.glob(::File.expand_path('../support/*.rb', __FILE__)).each { |f| require File.join(File.dirname(f), File.basename(f, '.rb')) }
+  ::Dir.glob(::File.expand_path('../support/**/*.rb', __FILE__)).each { |f| require File.join(File.dirname(f), File.basename(f, '.rb')) }
+else
+  ::Dir.glob(::File.expand_path('../support/*.rb', __FILE__)).each { |f| require_relative f }
+  ::Dir.glob(::File.expand_path('../support/**/*.rb', __FILE__)).each { |f| require_relative f }
+end
